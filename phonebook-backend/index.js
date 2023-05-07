2c688446f0e3c777fd0221e07f1c6eb64ca92d87
LLukasLL <l.fischer@tum.de>
2023-05-07 19:29:31 +0200

    fixes, error handling, delete

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -6,47 +6,15 @@ const mongoose = require('mongoose')
 const Person = require('./modules/person')
 
 const app = express();
+app.use(express.static('./build/'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 morgan.token('data', (req, res) => {
   return JSON.stringify(req.body);
 });
 app.use(cors())
-app.use(express.static('./build/'))
 
-/*
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    },
-    { 
-      "id": 5,
-      "name": "Lucasio", 
-      "number": "39-23-6423123"
-    }
-]
-*/
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+//---------------------------------------------------------------------------
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello There!</h1>')
@@ -65,17 +33,47 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
-  if (person) {response.json(person)}
-  else {response.status(404).end()}
+  const getid = Number(request.params.id);
+  // hier mongoose =>
+  const person = Person.findById(getid).then(person => {
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(404).end();
+    }
+  }).catch(error => {
+    console.log(error)
+    response.status(500).end()
+  })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+//---------------------------------------------------------------------------
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+//---------------------------------------------------------------------------
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  // hier mongoose
   const id = Number(request.params.id)
-  Person.findById(id).then(person => response.json(person))
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  Person.findByIdAndRemove(id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -83,17 +81,19 @@ app.post('/api/persons', (request, response) => {
   if (!body.name) response.status(403).json({ error: 'missing name' })
   if (!body.number) response.status(403).json({ error: 'missing number' })
   if (body === undefined) response.status(400).json({ error: 'content missing' })
-  for (const person of persons) {
-    if (person.name === body.name ) {
-      return response.status(403).json({ error: 'name must be unique' });
+  Person.find({}).then(persons => {
+    for (const person of persons) {
+      if (person.name === body.name ) {
+        return response.status(403).json({ error: 'name must be unique' });
+      }
     }
-  }
-  const newPerson = new Person({
-    name: body.name,
-    number: body.number
-  })
-  Person.save().then(savedPerson => {
-    response.json(savedPerson);
+    const newPerson = new Person({
+      name: body.name,
+      number: body.number
+    })
+    newPerson.save()
+      .then(savedPerson => response.json(savedPerson))
+      .catch(error => next(error))
   })
 })
 
